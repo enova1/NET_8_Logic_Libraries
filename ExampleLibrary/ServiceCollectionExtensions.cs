c9db8c67c6a2d462f1edebea4fee58cd1d228ef4
enova1 <xxclearwaterxx@gmail.com>
2025-08-09 00:17:35 -0400

    added origin for Azure and decupupled the API from the DAL

---
+++ ExampleLibrary/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExampleLibrary
+{
+    /// <summary>
+    /// Provides extension methods for configuring services related to the ExampleLibrary.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddExampleLibrary(
+            this IServiceCollection services,
+            string? connectionString)
+        {
+            // Register the DAL here 
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(connectionString));
+
+            // Dependency Injection here
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<IEmployee, Employee>();
+
+            // Add logic layer services here too if you have them
+
+            return services;
+        }
+    }
+}
